2d80414fa24bef2b312c3366be394a912108612d
huseynahmadov-pb <huseyn.ahmadov@pashabank.az>
2024-07-22 10:39:31 +0400

    initial commit

---
+++ ms-product/src/main/kotlin/az/company/products/MsProductApplication.kt
@@ -0,0 +1,11 @@
+package az.company.products
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsProductApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsProductApplication>(*args)
+}
